383dd7c81a6542ce0138aade4136f3e1c0702642
foresterh <foresterh@gmail.com>
2013-12-06 22:20:54 -0800

    added redis

--- plugins/anytag.py
+++ plugins/anytag.py
@@ -1,25 +1,23 @@
 import random
-from util import hook
-
-list_items = {}
+from util import hook, storage
 
 @hook.regex(r'(add )(?P<item>[\w\d\s]*)( to )(the |my )?(?P<list>[\w\d]*)( list)?')
 def add_to_list(input, output):
     if input.groupdict():
         item = input.groupdict()["item"]
         list_name = input.groupdict()["list"]
-        if list_name in list_items:
-            list_items[list_name].append(item)
-        else:
-            list_items[list_name] = [item]
+        #if storage.has_key(list_name):
+        storage.add_to_list(list_name, item)
+        #else:
+        #    list_items[list_name] = [item]
         output.say("Added %s to %s choices" % (item, list_name))
 
 @hook.regex(r'(what\'s|when\'s|when is|get|view|search for|show) (on |my |me |the )*(everything)?(?P<request>[\w\d]*)( list)?')
 def search_list(input, output):
     if input.groupdict():
         list_name = input.groupdict()["request"]
-        if list_name in list_items:
-            output.say(", ".join(list_items[list_name]))
+        if storage.has_key(list_name):
+            output.say(", ".join(storage.get_list(list_name)))
         else:
             output.say("There's as many items on that list as there are friends in your phone.")
 
@@ -27,8 +25,8 @@ def search_list(input, output):
 def show_random_list_item(input, output):
     if input.groupdict():
         list_name = input.groupdict()["request"]
-        if list_name in list_items:
-            result = random.choice(list_items[list_name])
+        if storage.has_key(list_name):
+            result = random.choice(storage.get_list(list_name))
             output.say("I pick %s" % result)
         else:
             output.say("There's as many items on that list as there are friends in your phone.")
--- util/storage.py
+++ util/storage.py
@@ -1,1 +1,23 @@
-__author__ = 'jhouston'
+import redis
+
+server = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+def has_key(key):
+    return server.exists(key)
+
+
+def get_value(key):
+    return server.get(key)
+
+
+def set_value(key, value):
+    server.set(key, value)
+
+
+def add_to_list(key, value):
+    server.lpush(key,value)
+
+
+def get_list(key):
+    return server.lrange(key,0,-1)
