08017033acf36b7d86cc1379577c464a08a460b8
dkeka <mcpwnage4@gmail.com>
2013-12-13 11:38:35 -0800

    lunch plugin.

--- plugins/lunch.py
+++ plugins/lunch.py
@@ -1,9 +1,14 @@
-# from util import hook, web
-#
-# @hook.command
-# def lunch(bot_input, bot_output):
-#     bot_output.say("lunch time")
-#
-# @hook.regex(r'lunch for (*.)')
+from util import hook, web
+
+@hook.command
+def lunch(bot_input, bot_output):
+    rests = web.get_json("http://lunchpad.meteor.com/restaurants/trending")
+
+    for rest in rests:
+    	bot_output.say(rest["name"])
+    	for user in rest["users"]:
+    		bot_output.say("  " + user["email"])
+    	bot_output.say()
+
+# @hook.regex(r'where is (?P<user>.*) for lunch')
 # def eat_lunch(bot_input, bot_output):
-#     bot_output.say("lunch time")
