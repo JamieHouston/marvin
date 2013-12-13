fc9a27278e5a04302cd79ebe38a8b42f812cf232
dkeka <mcpwnage4@gmail.com>
2013-12-13 13:49:35 -0800

    fix lunch ports.

--- plugins/lunch.py
+++ plugins/lunch.py
@@ -5,13 +5,13 @@ def add_to_lunch(bot_input, bot_output):
 	username = bot_input.nick
 	email = bot_output.get_user_by_name(username)
 
-	result = web.get_json("http://lunchpad.hq.daptiv.com/restaurant/" + bot_input.groupdict()["rest"] + "/add/" + email)
+	result = web.get_json("http://lunchpad.hq.daptiv.com:3000/restaurant/" + bot_input.groupdict()["rest"] + "/add/" + email)
 	bot_output('added you to restaurant')
 
 
 @hook.regex(r'lunchbot.*')
 def lunch(bot_input, bot_output):
-    rests = web.get_json("http://lunchpad.hq.daptiv.com/restaurants/trending:3000")
+    rests = web.get_json("http://lunchpad.hq.daptiv.com:3000/restaurants/trending")
     to_say = []
 
     for rest in rests:
