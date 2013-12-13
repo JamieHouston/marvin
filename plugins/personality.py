1496c9f6dd76136dcb506769b319070ccb794486
foresterh <foresterh@gmail.com>
2013-12-13 14:45:42 -0800

    fixing personality

--- plugins/personality.py
+++ plugins/personality.py
@@ -19,6 +19,7 @@ def get_personality(bot_input, bot_output):
                 bot_personality = load_personality(personality_name)
                 if bot_personality:
                     bot_output.say("Personality Override. Loading " + personality_name)
+                    bot_output.responses = bot_personality
                 else:
                     bot_output.say("It seems that test subject, I mean {0} is no longer...available").format(personality_name)
             except:
