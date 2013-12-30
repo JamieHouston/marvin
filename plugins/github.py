4a2c64f6ade65641d3feaff9d4e188443c59e158
foresterh <foresterh@gmail.com>
2013-12-30 12:06:41 -0800

    don't have a clue, but I'm sure it was awesome

--- plugins/github.py
+++ plugins/github.py
@@ -1,12 +1,17 @@
-from util import hook
+from util import hook, storage
 from github import Github
 
 @hook.command
 def github(bot_input, bot_output):
+    ".github [username] -- gives a list of pending pull requests for user. Ommitting username uses last one passed in"
     found_requests = False
     if bot_input.input_string:
         github_name = bot_input.input_string
+        storage.set_value("github:%s" % bot_output.nick, github_name)
+    else:
+        github_name = storage.get_value("github:%s" % bot_output.nick)
 
+    if github_name:
         gi = Github(bot_input.credentials["login"], bot_input.credentials["password"])
         org = gi.get_organization("daptiv")
         repo = org.get_repo("ppm")
