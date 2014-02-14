0c9124ad55118d758b99b9e10f63470548ea1a26
Jamie Houston <foresterh@gmail.com>
2014-02-14 15:53:58 -0800

    added spa to github repo

--- plugins/github.py
+++ plugins/github.py
@@ -12,18 +12,31 @@ def github(bot_input, bot_output):
     else:
         github_name = storage.get_hash_value("github:users", bot_input.nick)
 
-    if github_name:
-        gi = Github(bot_input.credentials["login"], bot_input.credentials["password"])
-        org = gi.get_organization("daptiv")
-        repo = org.get_repo("ppm")
-        search_string = "[ ] @{0}".format(github_name)
-        open_pull_requests = repo.get_pulls("open")
-        if open_pull_requests:
-            for pull in open_pull_requests:
-                if search_string in pull.body:
-                    found_requests = True
-                    bot_output.say("{0} - {1}/files?w=1".format(pull.title, pull.html_url))
-        if not found_requests:
-            bot_output.say(random.choice(bot_output.responses["nothing_for_you"]))
-    else:
+    if not github_name:
         bot_output.say("Who the hell are you?  Try .github <username>.  And if you pass in angle brackets I will return you to sender, postage unpaid.")
+        return
+
+    gi = Github(bot_input.credentials["login"], bot_input.credentials["password"])
+    org = gi.get_organization("daptiv")
+    ppm_pulls = get_pull_requests(org.get_repo("ppm"))
+    spa_pulls = get_pull_requests(org.get_repo("ppmspa"))
+    if ppm_pulls:
+        bot_output.say('From PPM: ' + '\n'.join(ppm_pulls))
+        found_requests = True
+    if spa_pulls:
+        bot_output.say('From PPM: ' + '\n'.join(spa_pulls))
+        found_requests = True
+
+    if not found_requests:
+        bot_output.say(random.choice(bot_output.responses["nothing_for_you"]))
+
+
+def get_pull_requests(repo, github_name):
+    results = []
+    search_string = "[ ] @{0}".format(github_name)
+    open_pull_requests = repo.get_pulls("open")
+    if open_pull_requests:
+        for pull in open_pull_requests:
+            if search_string in pull.body:
+                results.append("{0} - {1}/files?w=1".format(pull.title, pull.html_url))
+    return results
