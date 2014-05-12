1cee2f84c74c49808eb421c790c8b273e224e614
Jamie Houston <foresterh@gmail.com>
2014-05-12 15:47:21 -0700

    added score to trivia

--- plugins/trivia.py
+++ plugins/trivia.py
@@ -29,11 +29,26 @@ science = {
     "In what century did the Dodo bird become extinct": "17",
     "Hg is the symbol for what element": "mercury",
     "How many land miles are there in a league": "3",
-    "How many kilograms are there in a short ton": "907"
+    "How many kilograms are there in a short ton": "907",
+    "Which body joint includes the patella": "knee",
+    "What name is given to male rhinoceroses": "bull",
+    "What fruit comes in Hass and Florida variety": "avocado",
+    "In what country would you most likely encounter a Tasmanian devil": "australia",
+    "What does an archer keep his arrows in": "quiver",
+    "What does a phonophobe fear": "noise",
+    "In which ocean is the area known as Polynesia located": "pacific",
+    "Who was the Roman demigod best known for his strength": "hercules",
+    "How many points is the outer white ring of an Olympic archery target worth": "1",
+    "What number is indicated by the Roman numeral 'D'": "500",
+    "What country is home to the original Highland Games": "scotland",
+    "Which continent has never hosted the Olympics": "antartica",
+    "What is the dot in the 'i' called": "tittle",
+    "Which US state is nicknamed the 'Old Dominion' state": "virginia"
 }
 
 current_trivia = {}
 user_points = {}
+list_name = "trivia:points"
 
 @hook.command
 def trivia(bot_input, bot_output):
@@ -55,7 +70,21 @@ def check_trivia(bot_input, bot_output):
     if current_trivia['question']:
         guess = textutils.sanitize_message(bot_input.input_string)
         if guess in current_trivia['answer']:
+            add_point(bot_input.nick)
             bot_output.say("That's correct, {0}.  The answer to {1} is {2}".format(bot_input.nick, current_trivia['question'], current_trivia['answer']))
             current_trivia['question'] = ''
         else:
             bot_output.say("WRONG!")
+
+def add_point(nick):
+    user_points[nick] = storage.get_hash_value(list_name, nick) or 0
+    storage.set_hash_value(list_name, nick, user_points[nick])
+
+@hook.regex(r'trivia score', run_always=True)
+def get_points(bot_input, bot_output):
+    messages = []
+    for user, points in user_points.iteritems():
+        messages.append("%s has %d" % (user,points))
+    bot_output.say("Current Score: ")
+    bot_output.say(", ".join(messages))
+
