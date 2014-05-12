b9586439b9d6f98d57c3c9ab4350e9e7089acade
Jamie Houston <foresterh@gmail.com>
2014-05-12 09:01:46 -0700

    fixed trivia module

--- plugins/trivia.py
+++ plugins/trivia.py
@@ -5,11 +5,22 @@ first_time = True
 
 science = {
     "What is September's birthstone": "sapphire",
-    "What is the minimum number of bars on an abacus": ["9", "nine"],
+    "What is the minimum number of bars on an abacus": "9",
     "How many carats is pure gold": "24",
     "What is January's birthstone": "garnet",
     "Approximately how many miles are there in a nautical mile": "1.15",
-    "What is a group of lions called": "pride"
+    "What is a group of lions called": "pride",
+    "How many canine teeth are there in a normal set of human teeth": "4",
+    "In what decade was the very first test-tube baby born": "1978",
+    "What period saw the first dinosaurs": "triassic",
+    "How many chromosomes does a cow have": "60",
+    "What percentage of air is composed of oxygen": "21",
+    "A male swan is called a what": "cob",
+    "What fiber-producing plant is attacked by the boll weevil": "cotton",
+    "What constellation is represented by the whale": "cetus",
+    "What color is a Great Egret": "white",
+    "What method of food preservation was invented for the British Navy in 1813": "canning",
+    "What astrological chart takes its name from a Greek word meaning 'circle of little animals'": "zodiak"
 }
 
 
