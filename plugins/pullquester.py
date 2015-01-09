e67a5d4c1221b1b5b5b07954abe9f94e8738f318
Steve Hayes <shayes@daptiv.com>
2015-01-09 12:32:43 -0800

    pull calls github.teamstatus atm. WIP

--- plugins/pullquester.py
+++ plugins/pullquester.py
@@ -1,10 +1,13 @@
 from util import hook
-import github
-import random
+from gitty import Github_Helper
+from target_process import Target_Process
 
 @hook.command
 def pull(bot_input, bot_output):
-    ".pull [team_name] -- creates a pull request."
+    ".pull [options] -- creates a pull request."
 
-    if bot_input.input_string:
-        team_name = bot_input.input_string.lower()
+    gh = Github_Helper(bot_input, bot_output)
+    #tp = Target_Process(bot_input.bot.credentials["target_process"]["url"], bot_input.bot.credentials["target_process"]["login"], bot_input.bot.credentials["target_process"]["password"])
+
+    gh.teamstatus()
+    #tp.get_object("user_story")
