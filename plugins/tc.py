612e0a412f41163d239782b422818bfdef7cb724
Jamie Houston <foresterh@gmail.com>
2015-01-16 13:35:57 -0800

    flushing out teamcity stuffs

---
+++ plugins/tc.py
@@ -0,0 +1,22 @@
+from apis.teamcity.api import TeamCityRESTApiClient
+from util import hook
+
+@hook.regex(r'(show all projects)', run_always=True)
+def teamcity(bot_input, bot_output):
+    tc = TeamCityRESTApiClient(bot_input.bot.credentials["team_city"]["login"], bot_input.bot.credentials["team_city"]["password"], bot_input.bot.credentials["team_city"]["server"])
+    tc.get_all_projects()
+    data = tc.get_from_server()
+
+    for project in data['project']:
+        bot_output.say(project['name'])
+
+
+@hook.regex('tc build (?P<build_id>\d+)', run_always=True)
+def tc_status(bot_input, bot_output):
+    input_argument = bot_input.inp["build_id"]
+
+    if input_argument:
+        tc = TeamCityRESTApiClient(bot_input.bot.credentials["team_city"]["login"], bot_input.bot.credentials["team_city"]["password"], bot_input.bot.credentials["team_city"]["server"])
+        tc.get_build_by_build_id(input_argument)
+        data = tc.get_from_server()
+        bot_output.say(data['statusText'])
