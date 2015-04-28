4556f438781f41ab1919ea8aecbb6b1557640b7c
Jamie Houston <foresterh@gmail.com>
2015-04-28 14:04:23 -0700

    updated twss

--- plugins/twss.py
+++ plugins/twss.py
@@ -1,7 +1,7 @@
 from util import hook
 import random
 
-@hook.regex("(big|small|long|hard|soft|mouth|face|good|fast|slow|in there|on there|in that|on that|wet|dry|suck|blow|jaw|all in|fit that|fit it|hurts|hot|huge|balls|stuck)")
+@hook.regex("(big|small|long|hard|soft|mouth|face|good|fast|slow|in there|on there|in that|on that|wet|dry|suck|blow|jaw|all in|fit that|fit it|hurts|hot|huge|balls|stuck|deep)")
 def twss(bot_input, bot_output):
-    if random.choice(range(3)) == 1:
+    if random.choice(range(10)) == 1:
         bot_output.say("THAT'S WHAT SHE SAID!")
