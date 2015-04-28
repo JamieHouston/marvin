8385cd8c5f52cc495aae02711cd433d3cb0ad71f
Jamie Houston <foresterh@gmail.com>
2015-04-28 16:22:07 -0700

    fixed ipaddress lookup

--- plugins/internal.py
+++ plugins/internal.py
@@ -4,7 +4,10 @@ from core import reload, config
 
 @hook.command
 def refresh(bot_input, bot_output):
-    bot_output.say("Refreshing Plugins")
-    reload.reload(bot_input.bot)
-    config.config(bot_input.bot)
-    bot_output.say("I'm all shiny and new now")
+    if bot_output.master.lower() in bot_input.nick.lower():
+        bot_output.say("Refreshing Plugins")
+        reload.reload(bot_input.bot)
+        config.config(bot_input.bot)
+        bot_output.say("I'm all shiny and new now")
+    else:
+        bot_output.say("I'm gonna refresh my fist into your face!")
--- plugins/ping.py
+++ plugins/ping.py
@@ -1,6 +1,38 @@
 from util import hook
 import random
 import socket
+import os
+
+if os.name != "nt":
+    import fcntl
+    import struct
+
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
+
+def get_lan_ip():
+    ip = socket.gethostbyname(socket.gethostname())
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = [
+            "eth0",
+            "eth1",
+            "eth2",
+            "wlan0",
+            "wlan1",
+            "wifi0",
+            "ath0",
+            "ath1",
+            "ppp0",
+            ]
+        for ifname in interfaces:
+            try:
+                ip = get_interface_ip(ifname)
+                break
+            except IOError:
+                pass
+    return ip
 
 @hook.command
 def ping(bot_input, bot_output):
@@ -12,5 +44,5 @@ def ping(bot_input, bot_output):
 def location(bot_input, bot_output):
     """.location -- get my ip address and hostname"""
     hostname = socket.gethostname()
-    ip = socket.gethostbyname(socket.gethostname())
+    ip = get_lan_ip()
     bot_output.say("{0} - {1}".format(hostname, ip))
