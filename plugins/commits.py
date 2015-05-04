4148b8767766347b595c95848be97a71d643786b
Jamie Houston <foresterh@gmail.com>
2015-05-04 16:27:08 -0700

    fixed formatting of commit user

--- plugins/commits.py
+++ plugins/commits.py
@@ -24,7 +24,7 @@ def commit(bot_input, bot_output):
     global last_commit
     user_input = bot_input.input_string
     if user_input and last_commit is not None:
-        answer = "It was @{0} ({1})".format(last_commit.html_url, last_commit.author.name)
+        answer = "It was @{0} ({1})\n{2}".format(last_commit.author.login, last_commit.author.name, last_commit.html_url)
         if user_input.lower() == "answer":
             bot_output.say(answer)
         else:
