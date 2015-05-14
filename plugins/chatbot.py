bc464895a087adcc7d293c342ab6562cba57f5b5
Jamie Houston <foresterh@gmail.com>
2015-05-14 12:50:51 -0700

    not sure

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -12,7 +12,7 @@ def chat(bot_input, bot_output):
     global chatty
     global talked_to
 
-    message = bot_input.input_string.replace(bot_output.nick.lower(), '{user_nick}')
+    message = bot_input.input_string.replace(bot_output.nick.lower(), '{user_nick}').replace('0','').replace('@','')
     response = chatty.get_response(message)
     if (bot_input.direct_message or talked_to or random.randrange(10) == 1) and response:
         bot_output.say(response)
