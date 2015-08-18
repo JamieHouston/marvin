ff912205e3596161e5b663b21eb22492b087646c
Jamie Houston <foresterh@gmail.com>
2015-08-18 09:26:18 -0700

    fixed web

--- util/web.py
+++ util/web.py
@@ -38,9 +38,6 @@ def get_json(url, username=None, password=None):
         return json.loads(data)
     return None
 
-def get_json_with_headers(url, headers):
-
-
 def get_raw(url, username=None, password=None):
     raw_request = request.build_request(url, username, password)
     page = request.urlopen(raw_request)
