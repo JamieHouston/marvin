eed7afc6b8baf1e121061580fa348c0a7ccd9882
Jamie Houston <foresterh@gmail.com>
2015-11-10 11:08:43 -0800

    mamma jokes

---
+++ plugins/mamma.py
@@ -0,0 +1,12 @@
+from util import web, hook
+import random
+
+@hook.command
+@hook.regex(r'mom')
+def mamma(bot_input, bot_output):
+    url = 'https://raw.githubusercontent.com/rdegges/yomomma-api/master/jokes.txt'
+    responses = web.get_text(url)
+    if responses and len(responses):
+        lines = responses.split('\n')
+        line = random.choice(lines)
+        bot_output.say(line)
